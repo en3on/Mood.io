fc7594c586350d0b4a16f8d810b2f530a75a03b0
Adam Ladell <theadamladell@gmail.com>
2019-03-05 09:26:16 +1100

    Added display_module with initial functions

---
+++ dream_journal_app.rb
@@ -0,0 +1,33 @@
+module Display
+  def start_up_screen()
+
+  end
+
+  def log_in_screen()
+
+  end
+
+  def register_screen()
+
+  end
+
+  def main_menu()
+
+  end
+
+  def journal_entry_creation()
+
+  end
+
+  def mood_list()
+
+  end
+
+  def delete_entry()
+
+  end
+
+  def journal_entry_list()
+
+  end
+end
