624c889b2c3f77f236ea458214231ef152e22c37
Adam Ladell <theadamladell@gmail.com>
2019-03-05 15:14:16 +1100

    Added full read/ write capability

--- mood_journal.rb
+++ mood_journal.rb
@@ -1,6 +1,11 @@
 require 'csv'
+require 'pry'
+
+@journal_entries_arr = []
 
 def get_journal_entry(mood_list_arr)
+@journal_entries_arr
+
   print("Title: ")
   title = gets.strip()
 
@@ -31,28 +36,62 @@ def get_journal_entry(mood_list_arr)
 
 end
 
-def save_journal_entry_to_disk(journal_entry)
-  content = journal_entry[:content].join(';')
 
-  File.open("journal_entries.csv", "a+").puts("#{journal_entry[:title]},#{content},#{journal_entry[:mood]}")
+def add_journal_entry_to_arr(journal_entry)
+  content = journal_entry[:content].join(';')
+  csv_text = "title,content,mood\n#{journal_entry[:title]},#{content},#{journal_entry[:mood]}"
+  csv_entry = CSV.parse(csv_text, :headers => true)
+
+  csv_entry.each { |row|
+    @journal_entries_arr << row.to_hash
+  }
 end
 
-def read_journal_entries_from_disk(file_name)
-  csv_text = File.read("journal_entries.csv")
-  csv = CSV.parse(csv_text, :headers => true)
 
-  journal_entries = []
+def save_journal_entries_arr_to_disk()
+  File.open("journal_entries.csv", "w") do |file|
+    file.puts("title,content,mood")
+    @journal_entries_arr.each { |journal|
+      file.puts(journal)
+    }
+  end
 
-  csv.each { |row|
-    row_data = row.to_hash 
+end
+
+#def save_journal_entry_to_disk(journal_entry)
+  #content = journal_entry[:content].join(';')
+
+  #File.open("journal_entries.csv", "a+").puts("#{journal_entry[:title]},#{content},#{journal_entry[:mood]}")
+#end
+
+#def read_journal_entries_from_disk(file_name)
+  #csv_text = File.read("journal_entries.csv")
+  #csv = CSV.parse(csv_text, :headers => true)
+
+  #journal_entries = []
+
+  #csv.each { |row|
+    #row_data = row.to_hash 
     
-    journal_entries << row_data
+    #journal_entries << row_data
+  #}
+#end
+
+def read_journal_entries_to_array()
+  @journal_entries_arr = []
+
+  File.open("journal_entries.csv").each_with_index { |row, index|
+    if index != 0
+      @journal_entries_arr << eval(row) 
+    end
   }
 end
 
 def display_list_of_entries(journal_entries_arr)
-  journal_entries_arr.each_with_index { |journal, index|
+  @journal_entries_arr.each_with_index { |journal, index|
     puts("#{index + 1}. #{journal['title']}")
   }
 end
 
+binding.pry
+var = true
