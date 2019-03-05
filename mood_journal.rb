623bae8869c044c73cbb1911f3c9fd4878ef1f76
David Bui <davidbui@Davids-MacBook-Pro.local>
2019-03-05 15:28:38 +1100

    created custom mood system

--- mood_journal.rb
+++ mood_journal.rb
@@ -1,76 +1,76 @@
-# def main_menu
-#     puts("Welcome to mood.IO")
-#     puts("Please select the main following options: ")
-#     puts("Press 1 - To add a journal entry")
-#     puts("Press 2 - To view your journal entries")
-#     puts("Press 3 - To select a mood of your choice and view the the journal entries for that mood")
-#     puts("Press 4 - To view all mood list")
-#     puts("Press 5 - To view your most used words for each journal entries")
-#     puts("Press 6 - Quit mood.IO ")
-#     user_input = gets().strip.to_i
-#     while (user_input != 1 && user_input != 2 && user_input != 3 && user_input != 4 && user_input != 5 && user_input != 6)
-#         puts("Error please select the main following options: ")
-#         puts("Press 1 - To add a journal entry")
-#         puts("Press 2 - To view your journal entries")
-#         puts("Press 3 - To select a mood of your choice and view the the journal entries for that mood")
-#         puts("Press 4 - To view all mood list")
-#         puts("Press 5 - To view your most used words for each journal entries")
-#         puts("Press 6 - Quit mood.IO ")
-#         user_input = user_input = gets().strip.to_i
-#     end
-#     if (user_input == 1)
-#         p 1
-#     elsif (user_input == 2)
-#         p 2
-#     elsif (user_input == 3)
-#         p 3
-#     elsif (user_input == 4)
-#         p 4
-#     elsif (user_input == 5)
-#         p 5
-#     elsif (user_input == 6)
-#         puts "Thank you for using mood.IO"
-#     end
-# end
+def main_menu
+    puts("Welcome to mood.IO")
+    puts("Please select the main following options: ")
+    puts("Press 1 - To add a journal entry")
+    puts("Press 2 - To view your journal entries")
+    puts("Press 3 - To select a mood of your choice and view the the journal entries for that mood")
+    puts("Press 4 - To view all mood list")
+    puts("Press 5 - To view your most used words for each journal entries")
+    puts("Press 6 - Quit mood.IO ")
+    user_input = gets().strip.to_i
+    while (user_input != 1 && user_input != 2 && user_input != 3 && user_input != 4 && user_input != 5 && user_input != 6)
+        puts("Error please select the main following options: ")
+        puts("Press 1 - To add a journal entry")
+        puts("Press 2 - To view your journal entries")
+        puts("Press 3 - To select a mood of your choice and view the the journal entries for that mood")
+        puts("Press 4 - To view all mood list")
+        puts("Press 5 - To view your most used words for each journal entries")
+        puts("Press 6 - Quit mood.IO ")
+        user_input = user_input = gets().strip.to_i
+    end
+    if (user_input == 1)
+        p 1
+    elsif (user_input == 2)
+        p 2
+    elsif (user_input == 3)
+        p 3
+    elsif (user_input == 4)
+        p 4
+    elsif (user_input == 5)
+        p 5
+    elsif (user_input == 6)
+        puts "Thank you for using mood.IO"
+    end
+end
 
-# def get_journal_entry()
-#   print("Title: ")
-#   title = gets.strip()
+def get_journal_entry()
+  print("Title: ")
+  title = gets.strip()
 
-#   puts()
-#   puts("Journal Entry: (Type EXIT on a new line, to finish entry)")
-#   puts
+  puts()
+  puts("Journal Entry: (Type EXIT on a new line, to finish entry)")
+  puts
 
-#   input = ''
+  input = ''
 
-#   lines = []
+  lines = []
 
-#   while input != "EXIT"
-#     input = gets.strip
-#     lines << input
-#   end
+  while input != "EXIT"
+    input = gets.strip
+    lines << input
+  end
 
-#   return {
-#     title: title,
-#     content: lines 
-#   }
+  return {
+    title: title,
+    content: lines 
+  }
 
-# end
+end
 
-# def save_journal_entry_to_disk(journal_entry)
-#   content = journal_entry[:content].join(';')
+def save_journal_entry_to_disk(journal_entry)
+  content = journal_entry[:content].join(';')
 
-#   File.open("journal_entries.csv", "a+").puts("#{journal_entry[:title]},#{content}")
-#   File.close
-# end
+  File.open("journal_entries.csv", "a+").puts("#{journal_entry[:title]},#{content}")
+  File.close
+end
 
-#journal_entry = get_journal_entry()
+journal_entry = get_journal_entry()
 
-#save_journal_entry_to_disk(journal_entry)
+save_journal_entry_to_disk(journal_entry)
 
 mood_list_arr = ["Happy", "Sad", "Chilled"]
 
-#def custom_mood(mood_list_arr)
+def custom_mood(mood_list_arr)
     puts("Press 1: To add a mood")
     puts("Press 2: To delete a mood")
     puts("Press 3: Go back to Homepage")
@@ -94,6 +94,7 @@ mood_list_arr = ["Happy", "Sad", "Chilled"]
     elsif (user_input_custom_mood == 3)
         puts "home page" 
     end
-    
-    print mood_list_arr
+end
+custom_mood(mood_list_arr)
+print mood_list_arr
         
