b700ce1dcfb5fdd961c84f23ef9171f20e42923b
Adam Ladell <theadamladell@gmail.com>
2019-03-05 19:48:59 +1100

    Added mood_counter functionality

--- mood_journal.rb
+++ mood_journal.rb
@@ -3,156 +3,274 @@ require 'csv'
 require 'pry'
 require 'date'
 
-@journal_entries_arr = []
 
-def main_menu
-    puts("Welcome to mood.IO")
-    puts("Please select the main following options: ")
-    puts("Press 1 - To add a journal entry")
-    puts("Press 2 - To view your journal entries")
-    puts("Press 3 - To select a mood of your choice and view the the journal entries for that mood")
-    puts("Press 4 - To view all mood list")
-    puts("Press 5 - To view your most used words for each journal entries")
-    puts("Press 6 - Quit mood.IO ")
-    user_input = gets().strip.to_i
-    while (user_input != 1 && user_input != 2 && user_input != 3 && user_input != 4 && user_input != 5 && user_input != 6)
-        puts("Error please select the main following options: ")
-        puts("Press 1 - To add a journal entry")
-        puts("Press 2 - To view your journal entries")
-        puts("Press 3 - To select a mood of your choice and view the the journal entries for that mood")
-        puts("Press 4 - To view all mood list")
-        puts("Press 5 - To view your most used words for each journal entries")
-        puts("Press 6 - Quit mood.IO ")
-        user_input = user_input = gets().strip.to_i
-    end
-    if (user_input == 1)
-        p 1
-    elsif (user_input == 2)
-        p 2
-    elsif (user_input == 3)
-        p 3
-    elsif (user_input == 4)
-        p 4
-    elsif (user_input == 5)
-        p 5
-    elsif (user_input == 6)
-        puts "Thank you for using mood.IO"
-    end
-end
-
-def get_journal_entry()
-  print("Title: ")
-  title = gets.strip()
-
-  puts()
-  puts("Journal Entry: (Type EXIT on a new line, to finish entry)")
-  puts
-
-  input = ''
-
-  lines = []
-
-  while input != "EXIT"
-    input = gets.strip
-    lines << input
+class Journal
+  attr_accessor(:journal_entries_arr, :mood_list)
+  def initialize
+    @journal_entries_arr = []
+    @mood_list = []
   end
 
-  puts
+  def main_menu
+    input = ""
+    while input != "6" 
+      puts `clear`
+      puts("Welcome to mood.IO!")
+      puts("Please select an option: ")
+      puts
 
-  puts("Choose a mood for this entry!")
-  # run mood_list_display function
-  mood = "Happy"
+      puts("[1] Add journal entry")
+      puts("[2] View Journal Entries")
+      puts("[3] Add or Delete Moods")
+      puts("[4] Show the most used moods")
+      puts("[5] Filter entries by mood")
+      puts("[6] Exit")
+      puts
 
-  lines.pop
+      input = gets.strip
 
-  return {
-    title: title,
-    content: lines,
-    mood: mood
-  }
+      case input
+      when "1"
+        journal = get_journal_entry(@mood_list)
+        add_journal_entry_to_arr(journal)
+        save_journal_entries_arr_to_disk()
+      when "2"
+        if @journal_entries_arr.length > 0
+          input = display_list_of_entries
+          show_content_of_entry(input)
+        else
+          puts("There are no entries!")
+          puts("Press enter to continue...")
+          gets
+        end
+      when "3"
+        custom_mood()
+      when "4"
+        get_most_used_moods() 
+        puts("Press enter to return...")
+        gets
+      when "5"
+        p 5
+      when "6"
+        puts("Thanks for using mood.IO! :)")
+        break
+      else
+        puts("Please enter a valid option!")
+      end
+    end
+  end
 
-end
+  def get_journal_entry(mood_list)
+    puts `clear`
+    puts("NEW JOURNAL ENTRY")
+    puts
+    puts
+    print("Title: ")
+    title = gets.strip()
+
+    puts()
+    puts("Journal Entry: (Type EXIT on a new line, to finish entry)")
+    puts
+
+    input = ''
+
+    lines = []
+
+    while input != "EXIT"
+      input = gets.strip
+      lines << input
+    end
+
+    puts
+
+    view_mood_list(mood_list)
+    puts
+    puts("Choose a mood for this entry! (Enter the mood name)")
+    puts
+    mood = gets.strip().capitalize
+
+    lines.pop
+
+    return {
+      title: title,
+      content: lines,
+      mood: mood
+    }
+
+  end
 
 
-def add_journal_entry_to_arr(journal_entry)
-  content = journal_entry[:content].join(';')
-  csv_text = "title,content,mood\n#{journal_entry[:title]},#{content},#{journal_entry[:mood]}"
-  csv_entry = CSV.parse(csv_text, :headers => true)
+  def add_journal_entry_to_arr(journal_entry)
+    content = journal_entry[:content].join(';')
+    csv_text = "title,content,mood\n#{journal_entry[:title]},#{content},#{journal_entry[:mood]}"
+    csv_entry = CSV.parse(csv_text, :headers => true)
 
-  csv_entry.each { |row|
-    @journal_entries_arr << row.to_hash
-  }
-end
+    csv_entry.each { |row|
+      @journal_entries_arr << row.to_hash
+    }
+  end
 
-def custom_mood(mood_list_arr)
+  def custom_mood()
+    puts `clear`
+    view_mood_list(@mood_list)
+    puts
     puts("Press 1: To add a mood")
     puts("Press 2: To delete a mood")
     puts("Press 3: Go back to Homepage")
     user_input_custom_mood = gets().strip.to_i
     while (user_input_custom_mood != 1 && user_input_custom_mood != 2 && user_input_custom_mood != 3)
-        puts("Error Error incorrect input")
-        puts("Press 1: To add a mood?")
-        puts("Press 2: To delete a mood")
-        puts("Press 3: Go back to Homepage")
-        user_input_custom_mood = gets().strip.to_i
+      puts `clear`
+      puts("Error incorrect input")
+      puts
+      view_mood_list(@mood_list)
+      puts("Press 1: To add a mood?")
+      puts("Press 2: To delete a mood")
+      puts("Press 3: Go back to Homepage")
+      user_input_custom_mood = gets().strip.to_i
     end
     if (user_input_custom_mood == 1)
-        puts "Type your mood:"
-        mood_input = gets().strip.capitalize
-        mood_list_arr << mood_input
+      puts `clear`
+      puts "Type your mood:"
+      puts
+      mood_input = gets().strip
+      @mood_list << mood_input.capitalize
     elsif (user_input_custom_mood == 2)
-        puts "Type a mood that you want to delete:"
-        puts mood_list_arr
-        delete_mood_input = gets().strip.capitalize
-        mood_list_arr.delete(delete_mood_input)
+      puts `clear`
+      puts "Type a mood that you want to delete:"
+      puts
+      puts
+      delete_mood_input = gets().strip.capitalize
+      @mood_list.delete(delete_mood_input)
     elsif (user_input_custom_mood == 3)
-        puts "home page" 
+      return
     end
-end
 
-def save_journal_entries_arr_to_disk()
-  File.open("journal_entries.csv", "w") do |file|
-    file.puts("title,content,mood")
-    @journal_entries_arr.each { |journal|
-      file.puts(journal)
+    write_mood_list_to_file
+  end
+
+  def save_journal_entries_arr_to_disk()
+    File.open("journal_entries.csv", "w") do |file|
+      file.puts("title,content,mood")
+      @journal_entries_arr.each { |journal|
+        file.puts(journal)
+      }
+    end
+
+  end
+
+  def read_journal_entries_to_array()
+    @journal_entries_arr = []
+
+    File.open("journal_entries.csv").each_with_index { |row, index|
+      if index != 0
+        @journal_entries_arr << eval(row) 
+      end
     }
   end
 
-end
+  def display_list_of_entries()
+    puts `clear`
+    @journal_entries_arr.each_with_index { |journal, index|
+      puts("#{index + 1}. #{journal['title']}")
+    }
+    puts
+    print("Please enter the number of the journal entry you would like to view: ")
+    while true
+      input = gets.strip
 
-def read_journal_entries_to_array()
-  @journal_entries_arr = []
+      if input.count('0-9') == input.length
+        if input.to_i <= @journal_entries_arr.length
+          return input.to_i
+        end
+      end
 
-  File.open("journal_entries.csv").each_with_index { |row, index|
-    if index != 0
-      @journal_entries_arr << eval(row) 
+      puts("Please enter a valid number!")
     end
-  }
+  end
+
+  def show_content_of_entry(user_selection)
+    journal = @journal_entries_arr[user_selection - 1]
+
+    lines = journal['content'].split(';')
+
+    puts `clear`
+    puts journal['title']
+    puts
+
+    lines.each { |line|
+      puts line
+    }
+
+    puts
+    puts
+    puts("Mood: #{journal['mood']}")
+    puts
+    puts "Press enter to return to main menu..."
+    gets
+  end
+
+
+  def view_mood_list(mood_list_arr)
+      mood_list_arr.each_with_index do |moodli, index|
+          puts "#{index + 1}: #{moodli}"
+      end
+  end
+
+  def write_mood_list_to_file()
+    File.open("mood_list.txt", "w") do |file|
+      @mood_list.each { |mood|
+        file.puts(mood.strip)
+      }
+    end 
+  end
+
+  def read_mood_list_from_file()
+    @mood_list = []
+    
+    file = File.open("mood_list.txt", "r")
+    file.each_line { |line|
+      @mood_list << line.strip
+    }
+
+    file.close
+
+  end
+
+  def get_most_used_moods()
+    mood_hash = {}
+
+    @mood_list.each { |mood|
+      mood_hash[mood] = 0
+    }
+
+    puts(mood_hash)
+
+    @journal_entries_arr.each { |journal|
+      mood_hash[journal['mood']] += 1
+    }
+
+    sorted_mood_hash = mood_hash.sort_by { |mood, count| count}.reverse
+
+    sorted_mood_hash.each { |mood, count|
+      puts("#{mood}: #{count}")
+    }
+  end
 end
 
-def display_list_of_entries()
-  @journal_entries_arr.each_with_index { |journal, index|
-    puts("#{index + 1}. #{journal['title']}")
-  }
-end
 
-def show_content_of_entry(user_selection)
-  journal = @journal_entries_arr[user_selection - 1]
 
-  lines = journal['content'].split(';')
+def main()
 
-  puts `clear`
-  puts journal['title']
-  puts
+  journal_app = Journal.new
 
-  lines.each { |line|
-    puts line
-  }
+  journal_app.read_mood_list_from_file()
+
+  journal_app.read_journal_entries_to_array
+
+  journal_app.main_menu()
+
 end
 
 
-def view_mood_list(mood_list_arr)
-    mood_list_arr.each_with_index do |moodli, index|
-        puts "#{index + 1}: #{moodli}"
-    end
-end
+main()
+
